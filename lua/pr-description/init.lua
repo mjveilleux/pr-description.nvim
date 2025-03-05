cf9ef950636ab0eedb62a0a46b1baefb5d4871d2
x-mason-veilleux <x.mason.veilleux@wbgconsultant.com>
2025-03-05 11:06:16 -0800

    initial commit

---
+++ lua/pr-description/init.lua
@@ -0,0 +1,18 @@
+-- Main module for pr-description
+local M = {}
+
+-- Plugin setup function called by lazy.nvim
+function M.setup(opts)
+  -- Merge default options with user options
+  opts = vim.tbl_deep_extend("force", require("pr-description.config").defaults, opts or {})
+  
+  -- Store options for later use
+  M.options = opts
+end
+
+-- Main function that prints the PR description
+function M.show_description()
+  print("here is my pr-description")
+end
+
+return M
---
+++ plugin/pr-description.lua
@@ -0,0 +1,10 @@
+-- This file is loaded automatically by Neovim
+if vim.g.loaded_pr_description then
+  return
+end
+vim.g.loaded_pr_description = true
+
+-- Create vim command
+vim.api.nvim_create_user_command("PRDescription", function()
+  require("pr-description").show_description()
+end, { nargs = 0 })
