7883d2d8c689775e9c68f3d15d33691a2be07d70
x-mason-veilleux <x.mason.veilleux@wbgconsultant.com>
2025-03-05 11:54:04 -0800

    unborking commands bad bad bad

--- plugin/pr-description.lua
+++ plugin/pr-description.lua
@@ -4,7 +4,26 @@ if vim.g.loaded_pr_description then
 end
 vim.g.loaded_pr_description = true
 
--- Create vim command
+-- Get the module
+local pr_desc = require("pr-description.nvim")
+
 vim.api.nvim_create_user_command("PRDescription", function()
-  require("pr-description.nvim").show_description()
+  pr_desc.show_description()
 end, { nargs = 0 })
+
+vim.api.nvim_create_user_command("PRCreateDesc", function()
+  pr_desc.create_pr_description()
+end, { nargs = 0 })
+
+vim.api.nvim_create_user_command("PRCleanupTags", function()
+  pr_desc.cleanup_discussion_tags_cmd()
+end, { nargs = 0 })
+
+-- You can optionally add keymaps here too
+vim.keymap.set("n", "<leader>PRD", function()
+  pr_desc.create_pr_description()
+end, { desc = "Create [P][R] [D]escription" })
+
+vim.keymap.set("n", "<leader>CDT", function()
+  pr_desc.cleanup_discussion_tags_cmd()
+end, { desc = "[C]leanup [D]iscussion [T]ags" })
